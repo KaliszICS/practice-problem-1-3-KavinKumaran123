c00eec3e911520f0e1aafeef937cdaf277a7c0d8
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-03-05 01:26:40 +0000

    Practice 3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,42 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static String readFile(String filename) {
+		String a = "";
+		try{
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+				a += scanner.nextLine()+"\n";
+			}
+			scanner.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File not found.");
+		}
+		return a;
 	}
-
+	public static String backwardsReadFile(String filename) {
+		String a = "";
+		try{
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+				a += scanner.nextLine()+"\n";
+			}
+			scanner.close();
+		}
+		catch(FileNotFoundException e){
+			return "";
+		}
+		String reversed = "";
+		for (int i = a.length()-1; i >= 0; i--){
+			reversed += a.charAt(i);
+		}
+		return reversed;
+	}
 	
 
 }
